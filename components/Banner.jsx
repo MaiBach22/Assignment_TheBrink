40f8bcc591b7864b79ee6ec4249d6ee1bc459b38
MaiBach22 <maibach2022@gmail.com>
2022-05-19 14:15:35 +0200

    add responsive

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,59 +1,82 @@
 import style from "../styles/Banner.module.css";
 import Image from "next/image";
-import img1 from "../public/images/Ba-Na-Hills.jpg";
-import img2 from "../public/images/SaiGon.jpg";
+import img1 from "../public/images/teapot.jpg";
+import img2 from "../public/images/valley.jpg";
+import img3 from "../public/images/city.jpg";
+import img4 from "../public/images/castle.jpg";
+import img5 from "../public/images/paris.jpg";
+import { useRef, useState, useEffect } from "react";
+const { motion } = require("framer-motion");
+
+const images = [img1, img2, img3, img4, img5];
 
 function Banner() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+  const imgs = [img1, img2];
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
   return (
     <section id={style.container}>
       <article>
-        <figure id={style.banner}>
-          <div id={style.img1} className={style.img}>
-            <Image
-              layout="fill"
-              objectFit="cover"
-              quality={100}
-              src={img1}
-              alt="Ba-Na-Hill Landscape"
-            />
-          </div>
-          <div id={style.img2} className={style.img}>
-            <Image
-              layout="fill"
-              objectFit="cover"
-              className={style.image}
-              src={img2}
-              alt="SaiGon Landscape"
-            />
-          </div>
-          <div id={style.img3} className={style.img}>
-            <Image
-              layout="fill"
-              objectFit="cover"
-              quality={100}
-              src={img1}
-              alt="Ba-Na-Hill Landscape"
-            />
-          </div>
-          <div id={style.img4} className={style.img}>
-            <Image
-              layout="fill"
-              objectFit="cover"
-              className={style.image}
-              src={img2}
-              alt="SaiGon Landscape"
-            />
-          </div>
-          <div id={style.img5} className={style.img}>
-            <Image
-              layout="fill"
-              objectFit="cover"
-              quality={100}
-              src={img1}
-              alt="Ba-Na-Hill Landscape"
-            />
-          </div>
-        </figure>
+        <motion.div ref={carousel} className={style.figure}>
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            id={style.banner}
+          >
+            <motion.div id={style.img1} className={style.img}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                src={img1}
+                alt="Tea Pot"
+                priority="true"
+              />
+            </motion.div>
+            <motion.div id={style.img2} className={style.img}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                className={style.image}
+                src={img2}
+                alt="valley"
+                placeholder="blur"
+              />
+            </motion.div>
+            <motion.div id={style.img3} className={style.img}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                src={img3}
+                alt="city Landscape"
+                placeholder="blur"
+              />
+            </motion.div>
+            <motion.div id={style.img4} className={style.img}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                className={style.image}
+                src={img4}
+                alt="castle Landscape"
+                placeholder="blur"
+              />
+            </motion.div>
+            <motion.div id={style.img5} className={style.img}>
+              <Image
+                layout="fill"
+                objectFit="cover"
+                src={img5}
+                alt="paris Landscape"
+                placeholder="blur"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.div>
       </article>
     </section>
   );
--- components/Intro.jsx
+++ components/Intro.jsx
@@ -2,8 +2,19 @@ import style from "../styles/Intro.module.css";
 import Image from "next/image";
 import img1 from "../public/images/Ba-Na-Hills.jpg";
 import img2 from "../public/images/SaiGon.jpg";
+import { useRef, useState, useEffect } from "react";
+
+const { motion } = require("framer-motion");
 
 function Intro() {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+  const imgs = [img1, img2];
+  useEffect(() => {
+    console.log(carousel.current.scrollWidth, carousel.current.offsetWidth);
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
   return (
     <section className={style.container}>
       <article className={style.contain}>
@@ -12,26 +23,43 @@ function Intro() {
           <h1>Utrum ut placerat nec, varius sit amet lacus.</h1>
         </header>
 
-        <figure className={style.figure}>
-          <div className={style.innercarousel}>
-            <div className={style.img}>
+        <motion.div ref={carousel} className={style.figure}>
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            className={style.innercarousel}
+          >
+            {imgs.map((im, index) => {
+              return (
+                <motion.div key={index} className={style.img}>
+                  <Image
+                    layout="fill"
+                    objectFit="cover"
+                    src={im}
+                    alt="Landscape"
+                    priority="true"
+                  />
+                </motion.div>
+              );
+            })}
+            {/* <motion.div className={style.img}>
               <Image
                 layout="fill"
                 objectFit="cover"
                 src={img1}
                 alt="Ba-Na-Hill Landscape"
               />
-            </div>
-            <div className={style.img}>
+            </motion.div>
+            <motion.div className={style.img}>
               <Image
                 layout="fill"
                 objectFit="cover"
                 src={img2}
                 alt="Ba-Na-Hill Landscape"
               />
-            </div>
-          </div>
-        </figure>
+            </motion.div> */}
+          </motion.div>
+        </motion.div>
       </article>
     </section>
   );
